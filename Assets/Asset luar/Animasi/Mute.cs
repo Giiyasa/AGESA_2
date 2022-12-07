92d101c6b70241b548ee5f294793e6e92de4d633
Giiyasa <makgilang90@gmail.com>
2022-12-08 00:30:55 +0700

    update

---
+++ Assets/Asset luar/Animasi/Mute.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+bool isMute;
+ 
+ public void Mute (){
+     isMute = ! isMute;
+     AudioListener.volume =  isMute ? 0 : 1;
+ }
+
+}
