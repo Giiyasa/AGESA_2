57ad4288a1d794e6465207664506429de795b8d3
Giiyasa <makgilang90@gmail.com>
2023-01-08 00:00:33 +0700

    update

--- Assets/Asset luar/Animasi/Transition.cs
+++ Assets/Asset luar/Animasi/Transition.cs
@@ -16,10 +16,8 @@ public class Transition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //As an example, we'll be using GetKey() to test out the transition
-        //between game scenes, so if you are implementing this with this code
-        //make sure to modify the code according to your needs.
-        if(Input.GetKey(KeyCode.Space))
+        
+          if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
             LoadLevel();
         }
