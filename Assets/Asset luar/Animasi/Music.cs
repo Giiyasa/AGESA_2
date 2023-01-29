1de934c8a743869757dbf5ed9b8eec39521f278b
Giiyasa <makgilang90@gmail.com>
2023-01-29 12:40:52 +0700

    scene Sound

--- Assets/Asset luar/Animasi/Music.cs
+++ Assets/Asset luar/Animasi/Music.cs
@@ -1,46 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour
 {
-    private static Music _instance;
- 
-    public static Music instance
+    public static Music Instance { get; set; }
+    public AudioSource audioMusic;
+    public AudioClip[] clipMusic;
+
+    private void Awake()
     {
-        get
-        {
-            if(_instance == null)
-            {
-                _instance = GameObject.FindObjectOfType<Music>();
- 
-                //Tell unity not to destroy this object when loading a new scene!
-                DontDestroyOnLoad(_instance.gameObject);
-            }
- 
-            return _instance;
-        }
-    }
- 
-    void Awake() 
-    {
-        if(_instance == null)
+        if (Instance == null)
         {
             //If I am the first instance, make me the Singleton
-            _instance = this;
-            DontDestroyOnLoad(this);
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            //If a Singleton already exists and you find
-            //another reference in scene, destroy it!
-            if(this != _instance)
-                Destroy(this.gameObject);
+            Destroy(gameObject);
         }
     }
- 
-    public void Play()
+
+
+    public void ChangeMusic()
     {
-        //Play some audio!
+        for (int i = 0; i < clipMusic.Length; i++)
+        {
+            if (i == SceneManager.GetActiveScene().buildIndex)
+            {
+                audioMusic.Stop();
+                audioMusic.clip = clipMusic[i];
+                audioMusic.Play();
+
+
+                break;
+            }
+
+        }
     }
 }
--- Assets/Asset luar/Animasi/scene.cs
+++ Assets/Asset luar/Animasi/scene.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class scene: MonoBehaviour
+public class scene : MonoBehaviour
 {
-   public void LoadScene(string sceneName){
-    SceneManager.LoadScene(sceneName);
-   }
+
+    void Start()
+    {
+
+   
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
